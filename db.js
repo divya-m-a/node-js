f1249473efae245346f9ef6da6411870c4f80285
divya-m-a <84970045+divya-m-a@users.noreply.github.com>
2021-05-28 17:16:27 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+const Sequelize = require('sequelize');
+
+require('dotenv').config()
+//Sequelize connection
+var connection = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
+    port: 3306,
+    dialect: 'mysql'
+  });
+
+
+  
