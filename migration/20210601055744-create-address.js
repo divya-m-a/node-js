3ceb63a4a292ff93dcf4a11a2b08d38a7caa3b87
divya-m-a <84970045+divya-m-a@users.noreply.github.com>
2021-06-01 17:57:55 +0530

    Add files via upload

---
+++ migration/20210601055744-create-address.js
@@ -0,0 +1,69 @@
+'use strict';
+module.exports = {
+ /* up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull :false
+      },
+      userId: {
+        type:DataTypes.INTEGER,
+        allowNull:false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },*/
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('address', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull :false
+      },
+      userId: {
+        type:DataTypes.INTEGER,
+        allowNull:false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+ 
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('address');
+  }
+};
