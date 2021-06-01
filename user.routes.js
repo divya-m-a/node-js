5d0f522031dbec06d6ee0c3b1bf57162aaf880f3
divya-m-a <84970045+divya-m-a@users.noreply.github.com>
2021-06-01 17:47:25 +0530

    Delete user.routes.js

--- user.routes.js
+++
@@ -1,8 +0,0 @@
-const tests = require("../controllers/user.controller.js");
-
-module.exports = app => {
-    app.post("/post-user-details",tests.postuserdetails);
-    app.post("/get-user-details",tests.getuserdetails);
-    app.post("/update-details",tests.updatedetails);
-    app.post("/delete-details",tests.deletedetails);
-};
